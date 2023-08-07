bca3675063ea49c01e608ad331b25732c51ecdc5
Sankalp Dubey <sankalpdubey152002@gmail.com>
2023-08-07 16:45:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './Component/Header';
+import Home from './Component/Home';
+function App() {
+  return (
+    <div>
+     <Header/>
+     <Home/>
+    </div>
+  );
+}
+
+export default App;
